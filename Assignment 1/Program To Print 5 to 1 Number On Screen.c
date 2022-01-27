09117d6eb48653f94a7965980daef5b6b63d1675
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-01-27 20:56:50 +0530

    Add files via upload

---
+++ Assignment 1/Program To Print 5 to 1 Number On Screen.c
@@ -0,0 +1,21 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+void Display()
+{
+  int i=0;
+      i=5;
+      
+      while(i>=1)
+      {
+         printf("\t %d ",i);
+         i--;
+      }
+}
+int main()
+{
+  
+  Display ();
+  
+  return 0;
+}
