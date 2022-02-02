3e2970ecdff8623ebb3d077b4de445fdd9107094
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-02-03 00:07:21 +0530

    Add files via upload

---
+++ Assignment 1/lb1.5.c
@@ -0,0 +1,22 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+void Accept(ino)
+{
+  int cnt = 0;
+  
+  for(cnt = 1;cnt <= ino;cnt++)
+  {
+      printf(" * ");
+  }
+  
+}
+
+int main()
+{
+  int ivalue = 0;
+      ivalue = 5;
+      
+  Accept(ivalue);
+  return 0;
+}
