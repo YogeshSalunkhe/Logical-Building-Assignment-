ce4c95f9ef211e7be0b79726e3769d56c3962421
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-02-23 21:01:54 +0530

    Add files via upload

---
+++ Assignment 2/lb2.5.c
@@ -0,0 +1,40 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+#define True 1
+#define False 0
+
+typedef int BOOL;
+
+BOOL cheack_even(int Ino)
+{
+  if(Ino % 2==0)
+  {
+    return True;
+  }
+  else 
+  {
+    return False;
+  }
+}
+
+int main()
+{
+  int Ivalue=0,bret = 0;
+  
+  printf("\n\t Enter Number => ");
+  scanf("%d",&Ivalue);
+  
+  bret = cheack_even(Ivalue);
+  
+  if(bret == True)
+  {
+    printf("\n\t Given Number Is Even.");
+  }
+  else 
+  {
+    printf("\n\t Given Number is Odd.");
+  }
+  
+  return 0;
+}
