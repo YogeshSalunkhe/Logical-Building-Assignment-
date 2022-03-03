14c5e9772a5575be5d1ee966490043528a80a955
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-03-03 22:31:09 +0530

    Add files via upload

---
+++ Assignment 3/Lb3.4.c
@@ -0,0 +1,27 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+void Display_Convert(char Cvalue)
+{
+   if(Cvalue >= 'A' && Cvalue <= 'Z')
+   {
+     printf(" %c ",Cvalue + 32);
+   }
+   else if(Cvalue >= 'a' && Cvalue <='z')
+   {
+     printf(" %c ",Cvalue - 32);
+   }
+  
+}
+
+int main()
+{
+  char Cvalue = '\0';
+  
+  printf("\n\t Enter A Character => ");
+  scanf("%c",&Cvalue);
+  
+  Display_Convert(Cvalue);
+  
+  return 0;
+}
