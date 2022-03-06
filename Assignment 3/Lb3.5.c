95ef380f1593d11d9bba1a1aa1a3c1e8a15b4dce
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-03-06 21:58:10 +0530

    Add files via upload

---
+++ Assignment 3/Lb3.5.c
@@ -0,0 +1,44 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+#include <conio.h>
+
+#define True 1;
+#define False 0;
+
+int cheakvowel(char ch)
+{
+  if(ch == 'A'||ch =='a'||ch == 'E'||ch == 'e'||ch == 'I'||ch == 'i'||ch == 'O'||ch == 'o'||ch == 'U'|| ch == 'u')
+  {
+    return True;
+  }
+  else
+  {
+    return False;
+  }
+}
+
+int main()
+{
+  char ch = '\0';
+  
+ typedef int  BOOL; 
+ 
+ BOOL ret = False;
+  
+  printf("\n\t Enter A Character => ");
+  scanf("%c",&ch);
+  
+  ret =cheakvowel(ch);
+  
+  if(ret == 1)
+  {
+    printf("\n\t Enter Character %c Is Vowel",ch);
+  }
+  else
+  {
+    printf("\n\t Given Character %c Is Not Vowel",ch);
+  }
+  
+  getch();
+  return 0;
+}
