2e77095191b5db7ab8d7c596632bb7c63f4740bc
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-06-06 22:20:47 +0530

    Add files via upload

---
+++ Assignment 7/lb7.1.c
@@ -0,0 +1,26 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+void pattern(iNo)
+{
+  int i;
+  if(iNo < 0)
+  {
+    iNo = 0 - iNo;
+  }
+  for(i = 1;i <= iNo;i++)
+  {
+    printf(" $  * ");
+  }
+}
+int main()
+{
+  int iValue = 0;
+  
+  printf("\n\t Enter A Value => ");
+  scanf("%d",&iValue);
+  
+  pattern(iValue);
+  
+  return 0;
+}
