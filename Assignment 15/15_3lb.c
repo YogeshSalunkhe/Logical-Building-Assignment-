ad61bf4874a9e38fa5b38d1b4bc2d81560f81571
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2022-07-16 22:53:47 +0530

    Add files via upload

---
+++ Assignment 15/15_3lb.c
@@ -0,0 +1,29 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+  int i =0,j = 0,ch = iCol;
+  
+  for(i = 1;i <= iRow;i++,ch = iCol)
+  {
+    for(j = 1;j <= iCol;j++)
+    {
+      printf(" %d ",ch);
+      ch--;
+    }
+    printf("\n");
+  }
+}
+
+int main()
+{
+  int iValue1 = 0,iValue2 = 0;
+  
+  printf("\n\t Enter Rows and Columns => ");
+  scanf("%d%d",&iValue1,&iValue2);
+  
+  Pattern(iValue1,iValue2);
+  
+  return 0;
+}
