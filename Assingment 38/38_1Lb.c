0c27963290ef291fdc36e75dc6e96e868cd5d957
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2023-02-14 23:33:27 +0530

    Add files via upload

---
+++ Assingment 38/38_1Lb.c
@@ -0,0 +1,45 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+void Pattern(char *Str)
+{
+     int R = 0,C = 0,N = 0;
+     char *P = NULL;
+     
+     for(N = 0;*Str != '\0';N++,*Str++);
+     
+     
+     R = N;
+     
+     *Str--;
+    
+     for(N = 1;N <= R;*Str--,N++);
+    
+     for(R = 1 ;R <= N;R++)
+     {
+         P = Str;
+         
+        for(C = 1;C <= N;C++,*P++)
+        {
+            printf("%3c",*P);
+           
+        }
+        
+        printf("\n");
+     }
+     
+     
+     
+}
+
+int main()
+{
+  char src[20]="";
+  
+  printf("\n\t Enter String => ");
+  gets(src);
+  
+  Pattern(src);
+  
+  return 0;
+}
