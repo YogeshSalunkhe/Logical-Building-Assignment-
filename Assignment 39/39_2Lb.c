221caed97954fcae956d77d58d5c449d8bbf5221
YogeshSalunkhe <93177830+YogeshSalunkhe@users.noreply.github.com>
2023-02-28 23:29:37 +0530

    Add files via upload

---
+++ Assignment 39/39_2Lb.c
@@ -0,0 +1,44 @@
+//Compiler version gcc  6.3.0
+#include <stdio.h>
+
+void Strlen(char *Str1,char *Str2)
+{
+     int cnt = 0;
+     
+     for(cnt = 0;*Str1 != '\0';*Str1++)
+     {
+         if(*Str1 >= 'A' && *Str1 <= 'Z')
+         {
+            cnt++;
+         }
+     }
+     
+     printf("\n\t %d",cnt);
+     
+     for(cnt = 0;*Str2 != '\0';*Str2++)
+     {
+         if(*Str2 >= 'A' && *Str2 <= 'Z')
+         {
+            cnt++;
+         }
+     }
+     
+     
+     printf("\n\t %d",cnt);
+     
+}
+
+int main()
+{
+  char csrc[20]="",src[20]="";
+  
+  printf("\n\t Enter String => ");
+  gets(csrc);
+  
+  printf("\n\t Enter String => ");
+  gets(src);
+  
+  Strlen(csrc,src);
+  
+  return 0;
+}
